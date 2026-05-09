62158355e1b8191eee9ed6c0d32688f1d11917b6
BuildTools <unconfigured@null.spigotmc.org>
2026-05-09 04:03:04 +0200

    Disable aggressive PWA caching which breaks Next.js App Router dynamic routes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  cacheOnFrontEndNav: false,
+  aggressiveFrontEndNavCaching: false,
   workboxOptions: {
     disableDevLogs: true,
   },
